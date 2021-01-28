0d47ad6f821cd13c5d929cdb9909a287d1ef528f
DonaLd-D <57483340+DonaLd-D@users.noreply.github.com>
2021-01-28 14:24:07 +0800

    java string

---
+++ src/com/company/jstring/StringFormat.java
@@ -0,0 +1,11 @@
+package com.company.jstring;
+
+import java.util.*;
+
+public class StringFormat {
+    public static void main(String[] args){
+        double e = Math.E;
+        System.out.format("%f%n", e);
+        System.out.format(Locale.CHINA  , "%-10.4f%n%n", e);  //指定本地为中国（CHINA）
+    }
+}
